f86cfd18499fc1b48f4f6ce3d2816d1d4b3c77e4
Islam Galal <islam.galal@mobidevlabs.com>
2024-10-07 12:22:02 +0300

    ....

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Routes, Route } from "react-router-dom";
+// import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import { CreateMeeting } from "./CreateMeeting";
 import { JoinMeeting } from "./JoinMeeting";
